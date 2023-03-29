abe419e2d4597d01da3844db13c77c3c37132ccc
iwantgohome <iwantgohome@iwantgohomeui-MacBookPro.local>
2023-03-30 08:39:25 +0900

    react start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontsize: 24,
+    padding: '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello name="react" color="blue"/>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
+    </>
   );
 }
 
